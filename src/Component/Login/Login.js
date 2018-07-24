07c678008bffe9c8191e306c232c734185e6847d
cuongpp9 <cuongpp9@gmail.com>
2018-07-24 17:48:49 +0700

    create app

---
+++ src/Component/Login/Login.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import '../../CSS/Login.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="login-layout">
+
+        {/* Left Login layout */}
+        <div className="left-layout">
+          <div className="get-out">
+            <div className="leftmid">
+              <div className="center-align">
+                <img src="/assets/pill-white-trans.png" alt="logo" className="cicle" />
+              </div>
+              <div className="center-align">
+                <img src="/assets/wordmark-white.png" alt="mark" className="medly" />
+              </div>
+              <h5 className="info">Prescriptions Made Easy</h5>
+            </div>
+          </div>
+        </div>
+
+        {/* rifht Login layout */}
+        <div className="login-form">
+          <div className="get-out">
+            <div className="rightmid">
+              <div className="center-align">
+                <img src="/assets/pill-white-trans.png" alt="logo" className="cicle" />
+              </div>
+              <div className="center-align">
+                <img src="/assets/wordmark-white.png" alt="mark" className="medly" />
+              </div>
+              <h5 className="info">Prescriptions Made Easy</h5>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="copy-right">
+          <h5 className="center-align">© 2017 Lindenwood Health, Inc</h5>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
