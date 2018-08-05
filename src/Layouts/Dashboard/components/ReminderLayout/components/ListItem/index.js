abe7f1508ce54c5022e03b5b1bf24ae85eceb5f5
cuongpp9 <cuongpp9@gmail.com>
2018-08-05 22:17:36 +0700

    Add CountDownTime

--- src/Layouts/Dashboard/components/ReminderLayout/components/ListItem/index.js
+++ src/Layouts/Dashboard/components/ReminderLayout/components/ListItem/index.js
@@ -1,43 +1,52 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import './stype.scss';
 import * as actions from './../../../../../../redux/actions';
+import Countdown from "react-countdown-now";
 
 class ListItem extends Component {
-    onDeleteItem = () =>{
+    componentDidMount() { }
+    onDeleteItem = () => {
         //console.log('this.props.ListReminder.id',this.props.ListReminder)
         this.props.onDeleteReminder(this.props.ListReminder.id);
     }
-    onEditReminderItem = () =>{
+    onEditReminderItem = () => {
         //console.log("item:",this.props.ListReminder)
-       this.props.onEditReminder(this.props.ListReminder);
+        this.props.onEditReminder(this.props.ListReminder);
     }
+    onShowNotification = () => <span>Reminder is done!</span>;
     render() {
-        var {ListReminder} = this.props;
+        var { ListReminder } = this.props;
+        var timerReminder = moment(ListReminder.dueDate).diff(moment(new Date()));
+        //console.log(result);
+        //console.log(moment.duration(result).as('millisecond'));
         return (
             <a className="collection-item">
-                <span className="itemReminder" onClick = {this.onEditReminderItem}>{ListReminder.reminderItem}</span>
-                <span className="deleteItem" onClick ={this.onDeleteItem}>&#x2715;</span><br/>
-                <em>{moment(new Date(ListReminder.dueDate)).fromNow()}</em>
+                <span className="itemReminder" onClick={this.onEditReminderItem}>{ListReminder.reminderItem}</span>
+                <span className="deleteItem" onClick={this.onDeleteItem}>&#x2715;</span><br />
+                {/* <em>{moment(new Date(ListReminder.dueDate)).fromNow()}</em> */}
+                <Countdown date={Date.now() + moment.duration(timerReminder).as('millisecond')}>
+                    <i>{this.onShowNotification()}</i>
+                </Countdown>
             </a>
         );
     }
 }
 
-const mapStateToProps = (state) =>{
-    return{
-    }
-  }
-  const mapDispatchToProps = (dispatch, props)=>{
+const mapStateToProps = (state) => {
     return {
-      onDeleteReminder: (id) => {
+    }
+}
+const mapDispatchToProps = (dispatch, props) => {
+    return {
+        onDeleteReminder: (id) => {
             dispatch(actions.deleteReminder(id));
         },
         onEditReminder: (reminderItem) => {
             dispatch(actions.editReminder(reminderItem));
         }
     }
-  }
+}
 
-export default connect(mapStateToProps, mapDispatchToProps) (ListItem);
+export default connect(mapStateToProps, mapDispatchToProps)(ListItem);
